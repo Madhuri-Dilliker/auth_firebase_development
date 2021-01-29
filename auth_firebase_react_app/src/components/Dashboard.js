fe0d714840b02f85aa5e242cb3b8d8455bab57f2
Madhuri-Dilliker <madhurid@getreadyguilford.org>
2021-01-29 11:41:52 -0500

    Dashboard component

--- auth_firebase_react_app/src/components/Dashboard.js
+++ auth_firebase_react_app/src/components/Dashboard.js
@@ -1,10 +1,25 @@
-import React from 'react'
-export default function Dashboard() {
+import React,{ useState } from 'react'
+import { Card, Button, Alert  } from 'react-bootstrap'
 
+
+export default function Dashboard() {
+    const [error, setError] = useState('')
+   function handleLogout(){
+
+   }
     
     return (
-        <div>Dashboard</div>
-        
+      <>
+       <Card>
+         <Card.Body>
+         <h2 className="text-center mb-4">Profile</h2>
+         {error && <Alert varient="danger">{error}</Alert>}
+         </Card.Body>
+       </Card>
+       <div className="w-100 text-center mt-2">
+            <Button variant="link" onClick={handleLogout}>Log Out</Button>
+        </div>
+      </>        
     )
 }
 
