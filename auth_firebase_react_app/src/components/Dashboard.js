65f7f404ed339847dabf219922705cafd574359f
Madhuri-Dilliker <madhurid@getreadyguilford.org>
2021-01-29 11:49:13 -0500

    Dashboard component with update profile link button

--- auth_firebase_react_app/src/components/Dashboard.js
+++ auth_firebase_react_app/src/components/Dashboard.js
@@ -1,9 +1,12 @@
 import React,{ useState } from 'react'
 import { Card, Button, Alert  } from 'react-bootstrap'
-
+import { useAuth } from '../contexts/AuthContext'
+import { Link } from 'react-router-dom'
 
 export default function Dashboard() {
-    const [error, setError] = useState('')
+   const [error, setError] = useState("")
+   const { currentUser } = useAuth()
+
    function handleLogout(){
 
    }
@@ -14,6 +17,8 @@ export default function Dashboard() {
          <Card.Body>
          <h2 className="text-center mb-4">Profile</h2>
          {error && <Alert varient="danger">{error}</Alert>}
+         <strong>Email:</strong> {currentUser.email}
+         <Link to="/update-profile" className="btn btn-primary w-100 mt-3">Update Profile</Link>
          </Card.Body>
        </Card>
        <div className="w-100 text-center mt-2">
